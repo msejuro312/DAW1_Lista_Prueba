3f0fa7ade1f3b80dd74265e6c2cbe7b1879c25d5
msejuro312 <msejuro312@prontonmail.com>
2026-07-16 19:48:10 -0500

    commit 3

--- src/main/java/pe/cibertec/controllers/UsuarioController.java
+++ src/main/java/pe/cibertec/controllers/UsuarioController.java
@@ -1,18 +1,38 @@
 package pe.cibertec.controllers;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.cibertec.entities.Usuario;
 import pe.cibertec.repository.UsuarioRepository;
 
-@RestController
-@RequestMapping
+import java.util.List;
 
+@RestController
+@RequestMapping ("/api/usuarios")
+
+//Constructor para inicializar repository
 public class UsuarioController {
 
     private final UsuarioRepository usuarioRepository;
 
     public UsuarioController(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
+    }
+
+    //Función para registrar un nuevo usuario
+    @PostMapping ("/registrar")
+    public ResponseEntity<Usuario> registrar(Usuario usuario){
+        Usuario usuarioRegistrado = usuarioRepository.save(usuario);
+        return ResponseEntity.ok(usuarioRegistrado);
+    }
+
+    //FUnción para mostrar todos los usuarios registrados
+    @GetMapping
+    public List<Usuario> listar(){
+        return usuarioRepository.findAll();
     }
 }
