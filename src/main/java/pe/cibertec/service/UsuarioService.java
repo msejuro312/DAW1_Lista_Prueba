3258edc729d6c4bdd029441dc86fbbff694d08ee
msejuro312 <msejuro312@protonmail.com>
2026-08-01 18:03:40 -0500

    01.08.2026 - fin de clase

---
+++ src/main/java/pe/cibertec/service/UsuarioService.java
@@ -0,0 +1,42 @@
+package pe.cibertec.service;
+
+import org.springframework.stereotype.Service;
+import pe.cibertec.entities.Usuario;
+import pe.cibertec.repository.UsuarioRepository;
+
+import java.util.List;
+import java.util.Stack;
+
+@Service
+public class UsuarioService {
+    private  final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public Usuario registar(Usuario usuario)
+        {
+            return usuarioRepository.save(usuario);
+        }
+
+    public List<Usuario> listar()
+    {
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario obtener(Long id)
+    {
+        return usuarioRepository.findById(id).orElse(null);
+    }
+
+    public Usuario login(String correo, String clave)
+    {
+        Usuario usuario = usuarioRepository.findByCorreo(correo).orElse(null);
+        if(usuario != null && usuario.getClave().equals(clave))
+        {
+            return usuario;
+        }
+        return null;
+    }
+}
